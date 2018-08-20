b88947c1c0d4635f4baef7a2b39d9e55a34f6620
Josh W Lewis <josh.w.lewis@gmail.com>
2018-08-20 18:26:28 -0500

    Add permissive hold

--- keyboards/ergodox_infinity/keymaps/jwlvak/keymap.c
+++ keyboards/ergodox_infinity/keymaps/jwlvak/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [QWER] = LAYOUT_ergodox(
   // left
-  KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_NUBS,
+  KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LT(SPCL,KC_F11),
   KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,
   KC_LCTL,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
   KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           ALL_T(KC_MINS),
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                   KC_HOME,
                                                                   KC_BSPC,        KC_DELT,        KC_END,
   // right
-  KC_NUHS,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
+  LT(SPCL,KC_F12),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
   KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           ALT_T(KC_BSLS),
                   KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        CTL_T(KC_QUOT),
   MEH_T(KC_EQL),  KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        SFT_T(KC_MINS),
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [DVOR] = LAYOUT_ergodox(
   // left
-  KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_NUBS,
+  KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LT(SPCL,KC_F11),
   KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,           KC_Y,           KC_LBRC,
   KC_LCTL,        KC_A,           KC_O,           KC_E,           KC_U,           KC_I,
   KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,           KC_X,           ALL_T(KC_MINS),
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                   KC_HOME,
                                                                   KC_BSPC,        KC_DELT,        KC_END,
   // right
-  KC_NUHS,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
+  LT(SPCL,KC_F12),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
   KC_RBRC,        KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           ALT_T(KC_SLSH),
                   KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           CTL_T(KC_MINS),
   MEH_T(KC_EQL),  KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           SFT_T(KC_BSLS),
@@ -61,16 +61,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [SPCL] = LAYOUT_ergodox(
   // left hand
-  KC_TRNS,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,
-  KC_TRNS,       KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        TG(DVOR),
-  KC_TRNS,       KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRV,
-  KC_TRNS,       KC_PERC,        KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,        EPRM,
-  KC_TRNS,       KC_MUTE,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+  KC_TRNS,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,
+  KC_TRNS,        KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        TG(DVOR),
+  KC_TRNS,        KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRV,
+  KC_TRNS,        KC_PERC,        KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,        EPRM,
+  KC_TRNS,        KC_MUTE,        KC_TRNS,        KC_TRNS,        KC_TRNS,
                                                                                   KC_MUTE,        BL_INC,
                                                                                                   BL_TOGG,
                                                                   KC_VOLD,        KC_VOLU,        BL_DEC,
   // right hand
-  KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_POWER,
+  KC_TRNS,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_POWER,
   TG(DVOR),       KC_P7,          KC_P8,          KC_P9,          KC_PPLS,        KC_PSLS,        KC_PWR,
                   KC_P4,          KC_P5,          KC_P6,          KC_PPLS,        KC_PAST,        KC_WAKE,
   EPRM,           KC_P1,          KC_P2,          KC_P3,          KC_PENT,        KC_PMNS,        KC_EJCT,
@@ -139,7 +139,6 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
     uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
@@ -158,5 +157,4 @@ void matrix_scan_user(void) {
             // none
             break;
     }
-
 };
