52c0fad5d6bf4308bd9ad05449a53359ba177a23
Josh W Lewis <josh.w.lewis@gmail.com>
2018-09-26 18:07:17 -0500

    Adjust layout

--- keyboards/ergodox_infinity/keymaps/jwlvak/keymap.c
+++ keyboards/ergodox_infinity/keymaps/jwlvak/keymap.c
@@ -3,9 +3,9 @@
 #include "action_layer.h"
 #include "version.h"
 
-#define DVOR 0 // dvorak
-#define QWER 1 // qwerty
-#define SPCL 2 // symbols
+#define DV 0 // dvorak
+#define QW 1 // qwerty
+#define SL 2 // special/symbols
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -16,42 +16,42 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[DVOR] = LAYOUT_ergodox(
+[DV] = LAYOUT_ergodox(
   // left
   KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_F11,
-  KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,           KC_Y,           KC_LBRC,
+  KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,           KC_Y,           KC_F13,
   KC_LCTL,        KC_A,           KC_O,           KC_E,           KC_U,           KC_I,
-  KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,           KC_X,           KC_MINS,
-  KC_ESC,         KC_INS,         KC_LALT,        MO(SPCL),       KC_LGUI,
-                                                                                  KC_PAUSE,       KC_SLCK,
+  KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,           KC_X,           KC_F15,
+  KC_ESC,         LT(SL,KC_LBRC), LT(SL,KC_RBRC), KC_LALT,        KC_LGUI,
+                                                                                  KC_PAUSE,       KC_INS,
                                                                                                   KC_HOME,
                                                                   KC_BSPC,        KC_DELT,        KC_END,
   // right
-  LT(SPCL,KC_F12),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
-  ALL_T(KC_RBRC), KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           ALT_T(KC_SLSH),
-                  KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           CTL_T(KC_MINS),
-  MEH_T(KC_EQL),  KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           SFT_T(KC_BSLS),
+  LT(SL,KC_F12),  KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
+  ALL_T(KC_F14),  KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           RALT_T(KC_SLSH),
+                  KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           RCTL_T(KC_MINS),
+  MEH_T(KC_F16),  KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           RSFT_T(KC_BSLS),
                                   KC_RGUI,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,
   KC_MENU,        KC_APP,
   KC_PGUP,
   KC_PGDN,        KC_ENT,         KC_SPC
 ),
 
-[QWER] = LAYOUT_ergodox(
+[QW] = LAYOUT_ergodox(
   // left
   KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_F11,
-  KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,
+  KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F13,
   KC_LCTL,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
-  KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_MINS,
-  KC_ESC,         KC_INS,         KC_LALT,        MO(SPCL),       KC_LGUI,
-                                                                                  KC_PAUSE,       KC_SLCK,
+  KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_F15,
+  KC_ESC,         LT(SL,KC_LBRC), LT(SL,KC_RBRC), KC_LALT,        KC_LGUI,
+                                                                                  KC_PAUSE,       KC_INS,
                                                                                                   KC_HOME,
                                                                   KC_BSPC,        KC_DELT,        KC_END,
   // right
-  LT(SPCL,KC_F12),KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
-  ALL_T(KC_RBRC), KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           ALT_T(KC_BSLS),
-                  KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        CTL_T(KC_QUOT),
-  MEH_T(KC_EQL),  KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        SFT_T(KC_MINS),
+  LT(SL,KC_F12),  KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQL,
+  ALL_T(KC_F14),  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           RALT_T(KC_BSLS),
+                  KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        RCTL_T(KC_QUOT),
+  MEH_T(KC_F16),  KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        RSFT_T(KC_MINS),
                                   KC_RGUI,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,
   KC_MENU,        KC_APP,
   KC_PGUP,
@@ -59,19 +59,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 
-[SPCL] = LAYOUT_ergodox(
+[SL] = LAYOUT_ergodox(
   // left hand
-  KC_TRNS,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,
-  KC_TRNS,        KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        TG(DVOR),
+  KC_CAPS,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,
+  KC_TRNS,        KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        TG(QW),
   KC_TRNS,        KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRV,
   KC_TRNS,        KC_PERC,        KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,        EPRM,
-  KC_TRNS,        KC_MUTE,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+  KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
                                                                                   KC_MUTE,        BL_INC,
                                                                                                   BL_TOGG,
                                                                   KC_VOLD,        KC_VOLU,        BL_DEC,
   // right hand
   KC_TRNS,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_POWER,
-  TG(DVOR),       KC_P7,          KC_P8,          KC_P9,          KC_PPLS,        KC_PSLS,        KC_PWR,
+  TG(QW),         KC_P7,          KC_P8,          KC_P9,          KC_PPLS,        KC_PSLS,        KC_PWR,
                   KC_P4,          KC_P5,          KC_P6,          KC_PPLS,        KC_PAST,        KC_WAKE,
   EPRM,           KC_P1,          KC_P2,          KC_P3,          KC_PENT,        KC_PMNS,        KC_EJCT,
                                   KC_PDOT,        KC_P0,          KC_PENT,        KC_TRNS,        VRSN,
