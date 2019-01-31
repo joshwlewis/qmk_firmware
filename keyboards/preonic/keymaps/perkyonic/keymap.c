d0b87f026541287af318c8ff4135ffbad75584a7
Josh W Lewis <josh.w.lewis@gmail.com>
2019-01-31 15:09:17 -0600

    Swap enter and shift

--- keyboards/preonic/keymaps/perkyonic/keymap.c
+++ keyboards/preonic/keymaps/perkyonic/keymap.c
@@ -23,27 +23,22 @@ enum preonic_layers {
   PRK
 };
 
-enum preonic_keycodes {
-  DVORAK = SAFE_RANGE,
-  QWERTY,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [DVK] = LAYOUT_preonic_grid( \
-  KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,  \
-  KC_LCTL,          KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-  KC_LBRC,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RBRC,  \
-  LT(PRK, KC_BSLS), KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+  KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,  \
+  KC_LCTL,          KC_A,    KC_O,    KC_E,    KC_U,   KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
+  KC_LBRC,          KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RBRC,  \
+  LT(PRK, KC_BSLS), KC_ESC,  KC_LALT, KC_LGUI, KC_ENT, KC_BSPC, KC_SPC,  KC_RSFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [QRT] = LAYOUT_preonic_grid( \
-  KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  \
-  KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LBRC,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,  \
-  LT(PRK, KC_BSLS),  KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+  KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  \
+  KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LBRC,           KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,  \
+  LT(PRK, KC_BSLS),  KC_ESC,  KC_LALT, KC_LGUI, KC_ENT, KC_BSPC, KC_SPC, KC_RSFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [PRK] = LAYOUT_preonic_grid( \
@@ -51,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   AG_TOGG, _______, _______, _______, _______, _______, _______, TERM_OFF, RESET,   DEBUG,   TERM_ON, AU_TOG,  \
   _______, _______, _______, _______, _______, KC_MPRV, KC_MRWD, KC_VOLD,  KC_VOLU, KC_MFFD, KC_MNXT, MU_TOG, \
   _______, DF(QRT), _______, _______, _______, _______, _______, _______,  _______, _______, DF(DVK), CK_TOGG, \
-  _______, _______, _______, _______, _______, KC_DEL,  KC_MPLY, KC_CAPS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+  _______, _______, _______, _______, KC_CAPS, KC_DEL,  KC_MPLY, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 )
 
 };
