889ab371b3524d242f10cb98f69c1ed78520fdb5
Josh W Lewis <josh.w.lewis@gmail.com>
2019-01-30 20:08:29 -0600

    Get perkyonic working

--- keyboards/preonic/keymaps/perkyonic/keymap.c
+++ keyboards/preonic/keymaps/perkyonic/keymap.c
@@ -18,129 +18,40 @@
 #include "muse.h"
 
 enum preonic_layers {
-  _DVORAK,
-  _QWERTY,
-  _PERKY
+  DVK,
+  QRT,
+  PRK
 };
 
 enum preonic_keycodes {
   DVORAK = SAFE_RANGE,
   QWERTY,
-  PERKY,
-  BACKLIT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_DVORAK] = LAYOUT_preonic_grid( \
+[DVK] = LAYOUT_preonic_grid( \
   KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
   KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,  \
   KC_LCTL,          KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   KC_LBRC,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RBRC,  \
-  LT(KC_BSLS, MOD), KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  LT(PRK, KC_BSLS), KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
-[_QWERTY] = LAYOUT_preonic_grid( \
+[QRT] = LAYOUT_preonic_grid( \
   KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
   KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  \
   KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LBRC,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,  \
-  LT(KC_BSLS, MOD),  KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  LT(PRK, KC_BSLS),  KC_ESC,  KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
-[_PERKY] = LAYOUT_preonic_grid( \
+[PRK] = LAYOUT_preonic_grid( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  AG_TOGG, _______, _______, _______, _______, _______, _______, TERM_OFF, RESET,   DEBUG,   TERM_ON, AU_TOGG,  \
-  _______, _______, _______, _______, _______, KC_MPRV, KC_MRWD, KC_VOLD,  KC_VOLU, KC_MFFD, KC_MNXT, MU_TOGG, \
-  _______, QWERTY,  _______, _______, _______, _______, _______, _______,  _______, _______, DVORAK,  CK_TOGG, \
+  AG_TOGG, _______, _______, _______, _______, _______, _______, TERM_OFF, RESET,   DEBUG,   TERM_ON, AU_TOG,  \
+  _______, _______, _______, _______, _______, KC_MPRV, KC_MRWD, KC_VOLD,  KC_VOLU, KC_MFFD, KC_MNXT, MU_TOG, \
+  _______, DF(QRT), _______, _______, _______, _______, _______, _______,  _______, _______, DF(DVK), CK_TOGG, \
   _______, _______, _______, _______, _______, KC_DEL,  KC_MPLY, KC_CAPS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 )
 
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-        case QWERTY:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case DVORAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_DVORAK);
-          }
-          return false;
-          break;
-      }
-    return true;
-};
-
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
-
-void encoder_update(bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
-    } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    }
-  }
-}
-
-void dip_update(uint8_t index, bool active) {
-  switch (index) {
-    case 0:
-      if (active) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      break;
-    case 1:
-      if (active) {
-        muse_mode = true;
-      } else {
-        muse_mode = false;
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-        #endif
-      }
-   }
-}
-
-void matrix_scan_user(void) {
-  #ifdef AUDIO_ENABLE
-    if (muse_mode) {
-      if (muse_counter == 0) {
-        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-        if (muse_note != last_muse_note) {
-          stop_note(compute_freq_for_midi_note(last_muse_note));
-          play_note(compute_freq_for_midi_note(muse_note), 0xF);
-          last_muse_note = muse_note;
-        }
-      }
-      muse_counter = (muse_counter + 1) % muse_tempo;
-    }
-  #endif
-}
