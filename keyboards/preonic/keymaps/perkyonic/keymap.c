8f7ff5ac95a5f56c74ff3e7ab1f4b0065427e682
Josh W Lewis <josh.w.lewis@gmail.com>
2021-02-03 16:19:54 -0600

    Swap braces for OS dvorak mode; drop soft enter

--- keyboards/preonic/keymaps/perkyonic/keymap.c
+++ keyboards/preonic/keymaps/perkyonic/keymap.c
@@ -26,18 +26,18 @@ enum preonic_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [QRT] = LAYOUT_preonic_grid( \
-  KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,             KC_5,        KC_6,        KC_7,              KC_8,         KC_9,        KC_0,        KC_EQL,   \
-  KC_TAB,      KC_Q,        KC_W,        KC_E,        KC_R,             KC_T,        KC_Y,        KC_U,              KC_I,         KC_O,        KC_P,        KC_MINS,  \
+  KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,             KC_5,        KC_6,        KC_7,              KC_8,         KC_9,        KC_0,        KC_RBRC,   \
+  KC_TAB,      KC_Q,        KC_W,        KC_E,        KC_R,             KC_T,        KC_Y,        KC_U,              KC_I,         KC_O,        KC_P,        KC_LBRC,  \
   KC_ESC,      KC_A,        KC_S,        KC_D,        KC_F,             KC_G,        KC_H,        KC_J,              KC_K,         KC_L,        KC_SCLN,     KC_QUOT,  \
-  KC_LSFT,     KC_Z,        KC_X,        KC_C,        KC_V,             KC_B,        KC_N,        KC_M,              KC_COMM,      KC_DOT,      KC_SLSH,     KC_SFTENT,   \
-  KC_LCTL,     KC_BSLS,     KC_LALT,     KC_LGUI,     LT(PRK, KC_LBRC), KC_BSPC,     KC_SPC,      LT(PRK, KC_RBRC),  KC_LEFT,      KC_DOWN,     KC_UP,       KC_RGHT   \
+  KC_LSFT,     KC_Z,        KC_X,        KC_C,        KC_V,             KC_B,        KC_N,        KC_M,              KC_COMM,      KC_DOT,      KC_SLSH,     KC_ENT,   \
+  KC_LCTL,     KC_BSLS,     KC_LALT,     KC_LGUI,     LT(PRK, KC_MINS), KC_BSPC,     KC_SPC,      LT(PRK, KC_EQL),   KC_LEFT,      KC_DOWN,     KC_UP,       KC_RGHT   \
 ),
 
 [DVK] = LAYOUT_preonic_grid( \
   KC_GRV,      KC_1,        KC_2,        KC_3,        KC_4,             KC_5,        KC_6,        KC_7,              KC_8,         KC_9,        KC_0,        KC_EQL,   \
   KC_TAB,      KC_QUOT,     KC_COMM,     KC_DOT,      KC_P,             KC_Y,        KC_F,        KC_G,              KC_C,         KC_R,        KC_L,        KC_SLSH,  \
   KC_ESC,      KC_A,        KC_O,        KC_E,        KC_U,             KC_I,        KC_D,        KC_H,              KC_T,         KC_N,        KC_S,        KC_MINS,  \
-  KC_LSFT,     KC_SCLN,     KC_Q,        KC_J,        KC_K,             KC_X,        KC_B,        KC_M,              KC_W,         KC_V,        KC_Z,        KC_SFTENT,   \
+  KC_LSFT,     KC_SCLN,     KC_Q,        KC_J,        KC_K,             KC_X,        KC_B,        KC_M,              KC_W,         KC_V,        KC_Z,        KC_ENT,   \
   KC_LCTL,     KC_BSLS,     KC_LALT,     KC_LGUI,     LT(PRK, KC_LBRC), KC_BSPC,     KC_SPC,      LT(PRK, KC_RBRC),  KC_LEFT,      KC_DOWN,     KC_UP,       KC_RGHT   \
 ),
 
